2e686b152ac37cefaa82a955a02f445df1560dda
Irene Oppo <i.oppo@datocms.com>
2022-09-12 10:37:24 +0200

    Remove log line from dump

--- packages/cli-plugin-wordpress/wp_test_data/mysql/dump.sql
+++ packages/cli-plugin-wordpress/wp_test_data/mysql/dump.sql
@@ -1,4 +1,3 @@
-mysqldump: [Warning] Using a password on the command line interface can be insecure.
 -- MySQL dump 10.13  Distrib 5.7.38, for Linux (x86_64)
 --
 -- Host: localhost    Database: wordpress
@@ -15,7 +14,6 @@ mysqldump: [Warning] Using a password on the command line interface can be insec
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-mysqldump: Error: 'Access denied; you need (at least one of) the PROCESS privilege(s) for this operation' when trying to dump tablespaces
 
 --
 -- Table structure for table `wp_commentmeta`
