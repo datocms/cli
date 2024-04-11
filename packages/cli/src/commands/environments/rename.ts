a4bc024e8770d0f7a166889c75eb479fae96af61
Stefano Verna <s.verna@datocms.com>
2024-04-11 10:11:10 +0200

    Add environments:rename

---
+++ packages/cli/src/commands/environments/rename.ts
@@ -0,0 +1,32 @@
+import { type CmaClient, CmaClientCommand } from '@datocms/cli-utils';
+
+export default class Command extends CmaClientCommand<typeof Command.flags> {
+  static description = 'Renames an environment';
+
+  static args = [
+    {
+      name: 'ENVIRONMENT_ID',
+      description: 'The environment to rename',
+      required: true,
+    },
+    {
+      name: 'NEW_ENVIRONMENT_ID',
+      description: 'The new environment ID',
+      required: true,
+    },
+  ];
+
+  async run(): Promise<CmaClient.SimpleSchemaTypes.Environment> {
+    const { ENVIRONMENT_ID: oldId, NEW_ENVIRONMENT_ID: newId } =
+      this.parsedArgs;
+
+    this.startSpinner(`Renaming environment "${oldId}" -> "${newId}"`);
+    const result = await this.client.environments.rename(oldId, {
+      id: newId,
+    });
+
+    this.stopSpinner();
+
+    return result;
+  }
+}
