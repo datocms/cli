a04e6c454175cdc4641e5120d11739df4fd2b3c5
Irene Oppo <i.oppo@datocms.com>
2025-11-25 11:31:29 +0100

    Add slug field type + validations

--- packages/cli-plugin-contentful/src/utils/item-type-create-helpers.ts
+++ packages/cli-plugin-contentful/src/utils/item-type-create-helpers.ts
@@ -1,5 +1,9 @@
 import type { CmaClient } from '@datocms/cli-utils';
-import type { ContentFields, ContentTypeProps } from 'contentful-management';
+import type {
+  ContentFields,
+  ContentTypeProps,
+  EditorInterface,
+} from 'contentful-management';
 import { format } from 'date-fns';
 import { decamelize } from 'humps';
 import type { Context } from '../commands/contentful/import';
@@ -50,9 +54,15 @@ export const toFieldApiKey = (value: string): string => {
   return apiKey;
 };
 
-export function contentFieldTypeToDatoFieldType(field: ContentFields) {
+export function contentFieldTypeToDatoFieldType(
+  field: ContentFields,
+  editorInterface: EditorInterface,
+): CmaClient.ApiTypes.FieldCreateSchema['field_type'] {
   switch (field.type) {
     case 'Symbol':
+      if (isSlugField(field, editorInterface)) {
+        return 'slug';
+      }
       return 'string' as const;
     case 'Text':
       return 'text' as const;
@@ -146,6 +156,13 @@ export const findOrCreateStructuredTextAssetBlock = async (
   return contentfulAssetModularBlock.id;
 };
 
+export const getFieldControl = (
+  field: ContentFields,
+  editorInterface?: EditorInterface,
+) => {
+  return editorInterface?.controls?.find((c) => c.fieldId === field.id);
+};
+
 export const isMultipleLinksField = (
   field: ContentFields,
 ): boolean | undefined =>
@@ -163,6 +180,14 @@ export const isTitleField = (
 ): boolean | undefined =>
   field.id === contentType.displayField && field.type === 'Symbol';
 
+export const isSlugField = (
+  field: ContentFields,
+  editorInterface?: EditorInterface,
+): boolean | undefined => {
+  const control = getFieldControl(field, editorInterface);
+  return control?.widgetId === 'slugEditor';
+};
+
 type StringValidators = {
   required?: Record<string, never>;
   length?: {
@@ -177,6 +202,10 @@ type StringValidators = {
   format?: {
     custom_pattern: string | undefined;
   };
+};
+
+type SlugValidators = StringValidators & {
+  slug_title_field?: { title_field_id: string };
 };
 
 type IntegerValidators = {
@@ -216,8 +245,11 @@ type ArrayValidators = {
 
 export default function contentfulFieldValidatorsToDato(
   field: ContentFields,
+  editorInterface: EditorInterface,
+  datoFields: { [key: ContentFields['id']]: CmaClient.SimpleSchemaTypes.Field },
 ):
   | StringValidators
+  | SlugValidators
   | IntegerValidators
   | DateValidators
   | ArrayValidators
@@ -228,6 +260,10 @@ export default function contentfulFieldValidatorsToDato(
 
   switch (field.type) {
     case 'Symbol':
+      if (isSlugField(field, editorInterface)) {
+        return datoValidatorsForSlug(field, editorInterface, datoFields);
+      }
+      return datoValidatorsForString(field);
     case 'Text':
       return datoValidatorsForString(field);
     case 'Date':
@@ -308,6 +344,26 @@ const datoValidatorsForString = (field: ContentFields) => {
   }
 
   return datoValidators;
+};
+
+const datoValidatorsForSlug = (
+  field: ContentFields,
+  editorInterface: EditorInterface,
+  datoFields: { [key: string]: CmaClient.SimpleSchemaTypes.Field },
+): SlugValidators => {
+  const stringValidators = datoValidatorsForString(field);
+  const control = getFieldControl(field, editorInterface);
+
+  return {
+    ...stringValidators,
+    ...(control?.settings?.trackingFieldId &&
+      datoFields[control.settings.trackingFieldId as string] && {
+        slug_title_field: {
+          title_field_id:
+            datoFields[control.settings.trackingFieldId as string].id,
+        },
+      }),
+  };
 };
 
 const datoValidatorsForNumber = (field: ContentFields) => {
